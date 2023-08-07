1f173d10c03fb36e3922c973b8a8d5697107f9cb
SheepMaanDev <32809650+SheepMaan@users.noreply.github.com>
2023-08-07 16:44:47 +0200

    .

--- Portfolio/Back-end/routes/mail.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express')
-const router = express.Router()
-const mailController = require('../controllers/mail')
-
-router.post('/send-email', mailController.sendEmail)
-
-module.exports = router
